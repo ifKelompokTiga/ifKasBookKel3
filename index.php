c93753d64c211b880609e408b19c9a539745cc5e
nikkoahmad <nikkoahmad23@gmail.com>
2026-07-16 16:36:28 +0700

    update

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'includes/auth.php';
-requireLogin();
-
-$pageTitle = 'Nikko Kas - Buku Kas Digital';
-include 'includes/header.php';
-include 'includes/dashboard.php';
-include 'includes/footer.php';
